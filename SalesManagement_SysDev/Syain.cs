5a750809fed152237a9a6c9b61562d9beae57cef
真宏 岡嶋 <c2053@oic.jp>
2023-12-01 15:17:59 +0900

    社員管理画面各コントロールの作成

--- SalesManagement_SysDev/Syain.cs
+++ SalesManagement_SysDev/Syain.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SalesManagement_SysDev.Common;
+using SalesManagement_SysDev.CommonMethod;
+using SalesManagement_SysDev.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,111 @@ namespace SalesManagement_SysDev
 {
     public partial class Syain : UserControl
     {
+
+        private MessageDsp messageDsp = new MessageDsp();
+
         public Syain()
         {
             InitializeComponent();
+        }
+
+        private void Syain_Load(object sender, EventArgs e)
+        {
+            //各コントロールの初期設定
+            SetCtrlFormat();
+
+            //データの取得
+            if (!GetSelectData())
+            {
+                messageDsp.MessageBoxDsp("社員情報が獲得できませんでした", "エラー", MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private bool GetSelectData()
+        {
+            EmployeeDataAccess access = new EmployeeDataAccess();
+            //社員情報の全件取得
+            List<DispEmplyeeDTO> tb = access.GetEmployeeData();
+            if (tb == null)
+                return false;
+            //データグリッドビューへの設定
+            SetDataGridView(tb);
+            return true;
+        }
+
+        private void SetDataGridView(List<DispEmplyeeDTO> tb)
+        {
+            dataGridView1.DataSource = tb;
+            //列幅自動設定解除
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            //ヘッダーの高さ
+            dataGridView1.ColumnHeadersHeight = 50;
+            //ヘッダーの折り返し表示
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridView1.ColumnHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            //行単位選択
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            //ヘッダー文字位置、セル文字位置、列幅の設定
+            //社員ID
+            dataGridView1.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[0].Width = 30;
+            //社員名
+            dataGridView1.Columns[1].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[1].Width = 80;
+            //営業所ID(非表示)
+            dataGridView1.Columns[2].Visible = false;
+            //営業所名
+            dataGridView1.Columns[3].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[3].Width = 80;
+            //役職ID(非表示)
+            dataGridView1.Columns[4].Visible = false;
+            //役職名
+            dataGridView1.Columns[5].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[5].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[5].Width = 50;
+            //入社年月日
+            dataGridView1.Columns[6].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[6].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[6].Width = 120;
+            //パスワード
+            dataGridView1.Columns[7].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[7].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[7].Width = 120;
+            //電話番号
+            dataGridView1.Columns[8].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[8].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[8].Width = 55;
+            //社員管理フラグ(非表示)
+            dataGridView1.Columns[9].Visible = false;
+            //非表示理由(非表示)
+            dataGridView1.Columns[10].Visible = false;
+
+        }
+
+        private void SetCtrlFormat()
+        {
+            SalesOfficeDataAccess salesofficeDateAccess = new SalesOfficeDataAccess();
+            PositionDataAccess positionDataAccess = new PositionDataAccess();
+
+            //各テキストボックスに初期化(空白)
+            textBox_Syain_Namae.Text = "";
+            textBox_Syain_ID.Text = "";
+            textBox_Yakusyoku.Text = "";
+            textBox_Syain_Nenngetu.Text = "";
+            textBox_FAX.Text = "";
+            textBox_Pass.Text = "";
+
+            //各コンボボックスを初期化
+            comboBox_Eigyousyo.DisplayMember = "SoName";
+            comboBox_Eigyousyo.ValueMember = "SoID";
+            comboBox_Eigyousyo.DataSource = salesofficeDateAccess.GetSalesOfficeData();
+            comboBox_Eigyousyo.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox_Eigyousyo.SelectedIndex = -1;
         }
     }
 }
