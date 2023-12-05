ccc5e9f87058fb0eaf551376b3ff5dc8f82f5b89
真宏 岡嶋 <c2053@oic.jp>
2023-12-05 15:33:38 +0900

    社員管理画面一覧表示ボタン作成

--- SalesManagement_SysDev/Syain.Designer.cs
+++ SalesManagement_SysDev/Syain.Designer.cs
@@ -101,6 +101,7 @@
             this.button_Itirannhyouzi.TabIndex = 3;
             this.button_Itirannhyouzi.Text = "📖一覧表示";
             this.button_Itirannhyouzi.UseVisualStyleBackColor = false;
+            this.button_Itirannhyouzi.Click += new System.EventHandler(this.button_Itirannhyouzi_Click);
             // 
             // button_Sakuzyo
             // 
