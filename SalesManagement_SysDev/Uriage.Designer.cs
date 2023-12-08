467bb30f3cf7a5ef32dd43e446fbe813a216af2b
C2282@oic.osaka.jp <C2282@oic.osaka.jp>
2023-12-08 16:24:28 +0900

    競合解決

--- SalesManagement_SysDev/Uriage.Designer.cs
+++ SalesManagement_SysDev/Uriage.Designer.cs
@@ -96,6 +96,7 @@
             this.button_Sakuzyo.TabIndex = 2;
             this.button_Sakuzyo.Text = "🚮削除";
             this.button_Sakuzyo.UseVisualStyleBackColor = false;
+            this.button_Sakuzyo.Click += new System.EventHandler(this.button_Sakuzyo_Click);
             // 
             // button_Kuria
             // 
