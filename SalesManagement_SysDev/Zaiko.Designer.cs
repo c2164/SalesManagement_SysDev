2eb86a2e51e108465b9a94fd324c3f8d60f57f24
真宏 岡嶋 <c2053@oic.jp>
2023-12-21 14:47:39 +0900

    画面レイアウト変更

--- SalesManagement_SysDev/Zaiko.Designer.cs
+++ SalesManagement_SysDev/Zaiko.Designer.cs
@@ -113,7 +113,7 @@
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 11F);
+            this.label1.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(49, 231);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(136, 41);
@@ -122,7 +122,7 @@
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 11F);
+            this.label2.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label2.Location = new System.Drawing.Point(551, 232);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(133, 41);
@@ -131,7 +131,7 @@
             // 
             // label3
             // 
-            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 11F);
+            this.label3.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label3.Location = new System.Drawing.Point(1049, 230);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(134, 41);
@@ -140,7 +140,7 @@
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 11F);
+            this.label4.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label4.Location = new System.Drawing.Point(49, 377);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(107, 41);
