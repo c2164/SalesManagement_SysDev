54ac3509a33e40289371cb6b4f54cba972e3f3e2
2A28KS <c2164@oic.jp>
2023-12-22 13:37:43 +0900

    解決

--- SalesManagement_SysDev/Syain.Designer.cs
+++ SalesManagement_SysDev/Syain.Designer.cs
@@ -140,7 +140,7 @@
             this.label1.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(75, 172);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 41);
+            this.label1.Size = new System.Drawing.Size(106, 30);
             this.label1.TabIndex = 6;
             this.label1.Text = "社員名";
             // 
@@ -150,7 +150,7 @@
             this.label2.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label2.Location = new System.Drawing.Point(611, 172);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 41);
+            this.label2.Size = new System.Drawing.Size(100, 30);
             this.label2.TabIndex = 7;
             this.label2.Text = "社員ID";
             // 
@@ -160,7 +160,7 @@
             this.label3.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label3.Location = new System.Drawing.Point(1086, 171);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(138, 41);
+            this.label3.Size = new System.Drawing.Size(137, 30);
             this.label3.TabIndex = 8;
             this.label3.Text = "営業所名";
             // 
@@ -170,7 +170,7 @@
             this.label4.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label4.Location = new System.Drawing.Point(75, 285);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 41);
+            this.label4.Size = new System.Drawing.Size(106, 30);
             this.label4.TabIndex = 9;
             this.label4.Text = "役職名";
             // 
@@ -180,7 +180,7 @@
             this.label5.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label5.Location = new System.Drawing.Point(549, 288);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(168, 41);
+            this.label5.Size = new System.Drawing.Size(168, 30);
             this.label5.TabIndex = 10;
             this.label5.Text = "入社年月日";
             // 
@@ -190,7 +190,7 @@
             this.label6.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
             this.label6.Location = new System.Drawing.Point(46, 405);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(138, 41);
+            this.label6.Size = new System.Drawing.Size(137, 30);
             this.label6.TabIndex = 11;
             this.label6.Text = "電話番号";
             // 
@@ -198,9 +198,9 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("HGPｺﾞｼｯｸM", 11F, System.Drawing.FontStyle.Bold);
-            this.label8.Location = new System.Drawing.Point(1114, 405);
+            this.label8.Location = new System.Drawing.Point(1084, 303);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(127, 41);
+            this.label8.Size = new System.Drawing.Size(139, 30);
             this.label8.TabIndex = 13;
             this.label8.Text = "パスワード";
             // 
