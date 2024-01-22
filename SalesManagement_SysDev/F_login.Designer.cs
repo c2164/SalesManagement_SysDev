27c4a5d8fc55417b699e00dfcac9cedcce894b3d
2A28KS <c2164@oic.jp>
2024-01-22 21:29:03 +0900

    DB作成をボタンからタイトルラベル4回押下に変更

--- SalesManagement_SysDev/F_login.Designer.cs
+++ SalesManagement_SysDev/F_login.Designer.cs
@@ -40,7 +40,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button_logout = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button_CleateDatabase = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -52,20 +51,20 @@
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(0, 3);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Location = new System.Drawing.Point(0, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(230, 764);
+            this.panel1.Size = new System.Drawing.Size(299, 1019);
             this.panel1.TabIndex = 0;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.White;
             this.button3.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 16.125F, System.Drawing.FontStyle.Bold);
-            this.button3.Location = new System.Drawing.Point(8, 571);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button3.Location = new System.Drawing.Point(10, 761);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(215, 171);
+            this.button3.Size = new System.Drawing.Size(280, 228);
             this.button3.TabIndex = 3;
             this.button3.Text = "社員管理";
             this.button3.UseVisualStyleBackColor = false;
@@ -75,10 +74,10 @@
             // 
             this.button2.BackColor = System.Drawing.Color.White;
             this.button2.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 16.125F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(8, 355);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Location = new System.Drawing.Point(10, 473);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(217, 171);
+            this.button2.Size = new System.Drawing.Size(282, 228);
             this.button2.TabIndex = 2;
             this.button2.Text = "営業担当";
             this.button2.UseVisualStyleBackColor = false;
@@ -88,10 +87,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(10, 127);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Location = new System.Drawing.Point(13, 169);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(215, 171);
+            this.button1.Size = new System.Drawing.Size(280, 228);
             this.button1.TabIndex = 1;
             this.button1.Text = "物流担当";
             this.button1.UseVisualStyleBackColor = false;
@@ -107,68 +106,62 @@
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.button_logout);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.button_CleateDatabase);
-            this.panel2.Location = new System.Drawing.Point(3, 3);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel2.Location = new System.Drawing.Point(4, 4);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1465, 108);
+            this.panel2.Size = new System.Drawing.Size(1904, 144);
             this.panel2.TabIndex = 1;
             // 
             // label_loginEmPosition
             // 
             this.label_loginEmPosition.AutoSize = true;
-            this.label_loginEmPosition.Location = new System.Drawing.Point(236, 55);
-            this.label_loginEmPosition.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_loginEmPosition.Location = new System.Drawing.Point(307, 73);
             this.label_loginEmPosition.Name = "label_loginEmPosition";
-            this.label_loginEmPosition.Size = new System.Drawing.Size(0, 18);
+            this.label_loginEmPosition.Size = new System.Drawing.Size(0, 24);
             this.label_loginEmPosition.TabIndex = 7;
             // 
             // label_loginEmName
             // 
             this.label_loginEmName.AutoSize = true;
-            this.label_loginEmName.Location = new System.Drawing.Point(248, 36);
-            this.label_loginEmName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_loginEmName.Location = new System.Drawing.Point(322, 48);
             this.label_loginEmName.Name = "label_loginEmName";
-            this.label_loginEmName.Size = new System.Drawing.Size(0, 18);
+            this.label_loginEmName.Size = new System.Drawing.Size(0, 24);
             this.label_loginEmName.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(232, 11);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(302, 15);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 18);
+            this.label4.Size = new System.Drawing.Size(104, 24);
             this.label4.TabIndex = 5;
             this.label4.Text = "ログイン中";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(187, 55);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(243, 73);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.Size = new System.Drawing.Size(58, 24);
             this.label3.TabIndex = 4;
             this.label3.Text = "役職";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(180, 36);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(234, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.Size = new System.Drawing.Size(82, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "社員名";
             // 
             // button_logout
             // 
             this.button_logout.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 10F, System.Drawing.FontStyle.Bold);
-            this.button_logout.Location = new System.Drawing.Point(1282, 21);
-            this.button_logout.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button_logout.Location = new System.Drawing.Point(1667, 28);
+            this.button_logout.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button_logout.Name = "button_logout";
-            this.button_logout.Size = new System.Drawing.Size(150, 48);
+            this.button_logout.Size = new System.Drawing.Size(195, 64);
             this.button_logout.TabIndex = 4;
             this.button_logout.Text = "👤ログアウト";
             this.button_logout.UseVisualStyleBackColor = true;
@@ -178,42 +171,31 @@
             // 
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(200)))), ((int)(((byte)(230)))));
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 28.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(573, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(745, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(421, 70);
+            this.label1.Size = new System.Drawing.Size(547, 93);
             this.label1.TabIndex = 0;
             this.label1.Text = "販売在庫管理";
-            // 
-            // button_CleateDatabase
-            // 
-            this.button_CleateDatabase.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 10F, System.Drawing.FontStyle.Bold);
-            this.button_CleateDatabase.Location = new System.Drawing.Point(1459, 39);
-            this.button_CleateDatabase.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.button_CleateDatabase.Name = "button_CleateDatabase";
-            this.button_CleateDatabase.Size = new System.Drawing.Size(150, 48);
-            this.button_CleateDatabase.TabIndex = 2;
-            this.button_CleateDatabase.Text = "データベース作成";
-            this.button_CleateDatabase.UseVisualStyleBackColor = true;
-            this.button_CleateDatabase.Click += new System.EventHandler(this.btn_CleateDabase_Click);
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(230, 111);
+            this.panel3.Location = new System.Drawing.Point(299, 148);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1238, 656);
+            this.panel3.Size = new System.Drawing.Size(1609, 875);
             this.panel3.TabIndex = 2;
             // 
             // F_Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1457, 757);
+            this.ClientSize = new System.Drawing.Size(1894, 1009);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "F_Login";
             this.Text = "販売管理システム";
             this.Load += new System.EventHandler(this.F_Login_Load);
@@ -233,7 +215,6 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button_logout;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button_CleateDatabase;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label_loginEmPosition;
         private System.Windows.Forms.Label label_loginEmName;
